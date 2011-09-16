b8fa172053eb37c081fddb1b626526d321a679d0
Simone Tripodi <simonetripodi@apache.org>
2011-09-16 21:54:33 +0000

    removed trailing spaces
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/dbutils/trunk@1171818 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/dbutils/ResultSetHandler.java
+++ src/java/org/apache/commons/dbutils/ResultSetHandler.java
@@ -26,14 +26,14 @@ public interface ResultSetHandler<T> {
 
     /**
      * Turn the <code>ResultSet</code> into an Object.
-     * 
+     *
      * @param rs The <code>ResultSet</code> to handle.  It has not been touched
      * before being passed to this method.
-     * 
+     *
      * @return An Object initialized with <code>ResultSet</code> data. It is
-     * legal for implementations to return <code>null</code> if the 
+     * legal for implementations to return <code>null</code> if the
      * <code>ResultSet</code> contained 0 rows.
-     * 
+     *
      * @throws SQLException if a database access error occurs
      */
     T handle(ResultSet rs) throws SQLException;
