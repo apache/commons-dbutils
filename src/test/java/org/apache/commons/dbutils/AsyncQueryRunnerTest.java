e8d7343b9f85e620a0927f8d685f175b4404bf79
Sebastian Bazley <sebb@apache.org>
2011-11-23 18:08:36 +0000

    Unused import
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/dbutils/trunk@1205516 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/dbutils/AsyncQueryRunnerTest.java
+++ src/test/java/org/apache/commons/dbutils/AsyncQueryRunnerTest.java
@@ -19,7 +19,6 @@ package org.apache.commons.dbutils;
 import static org.junit.Assert.fail;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
