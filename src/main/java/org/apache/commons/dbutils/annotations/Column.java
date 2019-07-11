99071e60fbde76d6584946cc9e08fb97a2aac31f
rewerma <rewerma@163.com>
2019-07-12 03:21:24 +0800

    Add a annotation Column for mapping

---
+++ src/main/java/org/apache/commons/dbutils/annotations/Column.java
@@ -0,0 +1,13 @@
+package org.apache.commons.dbutils.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Column {
+
+    String name() default "";
+}
